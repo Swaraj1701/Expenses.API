a54272e9887e717f1384543f64fee943e491d309
Swaraj1701 <samanta.swaraj17@gmail.com>
2025-11-04 11:26:17 +0530

    enable swagger for prod

--- Expenses.API/Program.cs
+++ Expenses.API/Program.cs
@@ -41,11 +41,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 app.UseAuthentication();
